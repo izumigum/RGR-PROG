b8bef9e2cc577d20be0dc27a9ad9202f650f68f9
izumigum <154719003+izumigum@users.noreply.github.com>
2024-01-02 16:39:46 +0700

    RGR Makarova FI202.cpp

--- RGR Makarova FI202.cpp
+++ RGR Makarova FI202.cpp
@@ -1,10 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 #include "controller.h"
+#include <stdlib.h>
+#include <time.h>
 #include <windows.h>
 using namespace std;
 
 
 int main() {
+    setlocale(LC_ALL, "ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     controller ctrl;
